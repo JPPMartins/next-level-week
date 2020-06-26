3f2e650181a5e10d0b5b284cd7d1747a51db4d4a
João Paulo Peres Martins <joaopaulopemartins@gmail.com>
2020-06-26 15:16:30 -0300

    add home page in mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {  StatusBar, StyleSheet, Text, View } from 'react-native';
+import {AppLoading} from 'expo';
+import {  StatusBar, Text, View } from 'react-native';
 import Home from './src/pages/Home';
+import { Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium
+  });
+
+  if (!fontLoaded){
+    return <AppLoading />
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
       <Home />
     </>
-    
   );
 }
